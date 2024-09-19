09239a8939031f4f7a1beaa0cb79d5c0e3a9b9ed
Shivangi <shivangit1111@gmail.com>
2024-09-19 17:20:30 +0530

    style: define strings

--- app/src/main/java/com/example/loginflow/components/AppComponents.kt
+++ app/src/main/java/com/example/loginflow/components/AppComponents.kt
@@ -1,8 +1,30 @@
 package com.example.loginflow.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun NormalTextComponent(value:String){
+fun NormalTextComponent(value: String) {
+    Text(
+        text = value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 80.dp),
+        style = TextStyle(
+            fontSize = 18.sp,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.Normal,
+            fontStyle = FontStyle.Normal
 
+        )
+    )
 }
--- app/src/main/java/com/example/loginflow/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/loginflow/screens/SignUpScreen.kt
@@ -1,8 +1,22 @@
 package com.example.loginflow.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun  SignUpScreen(){
+fun SignUpScreen() {
+    Surface(color = Color.White,
+        modifier = Modifier.fillMaxSize()
+        ) {
 
+    }
+}
+@Preview
+@Composable
+fun DefaultPreviewOfSignUpScreen(){
+    SignUpScreen()
 }
