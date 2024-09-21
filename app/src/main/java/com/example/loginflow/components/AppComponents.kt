682061911dde4a05e36bb87c4b54854d2b4314b4
Shivangi <shivangit1111@gmail.com>
2024-09-21 17:52:16 +0530

    refactor: create textField component

--- app/src/main/java/com/example/loginflow/components/AppComponents.kt
+++ app/src/main/java/com/example/loginflow/components/AppComponents.kt
@@ -2,9 +2,22 @@ package com.example.loginflow.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -14,6 +27,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.loginflow.R
+import com.example.loginflow.componentShape
+import com.example.loginflow.ui.theme.Primary
+import com.example.loginflow.ui.theme.Secondary
+import com.example.loginflow.ui.theme.TextColor
 
 @Composable
 fun NormalTextComponent(value: String) {
@@ -34,8 +51,6 @@ fun NormalTextComponent(value: String) {
         color = colorResource(id = R.color.colorText),
 
         )
-
-
 }
 
 @Composable
@@ -57,6 +72,34 @@ fun HeadingTextComponent(value: String) {
         color = colorResource(id = R.color.colorText),
 
         )
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextField(labelValue: String) {
+    var textValue by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = textValue,
+        onValueChange = { textValue = it },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Primary,
+            focusedLabelColor = Primary,
+            cursorColor = Primary,
+            focusedTextColor = TextColor
+        ),
+        modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth()
+            .clip(componentShape.small),
 
+        label = { Text(text = "First Name") },
+        keyboardOptions = KeyboardOptions.Default,
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Outlined.AccountCircle,
+                contentDescription = "emailIcon"
+            )
+        },
+
+    )
 }
