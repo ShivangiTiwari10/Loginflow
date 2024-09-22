db78526459594486fe09f8b9ea621bc3f38a261e
Shivangi <shivangit1111@gmail.com>
2024-09-22 15:46:15 +0530

    feat: create component for checkbox

--- app/src/main/java/com/example/loginflow/components/AppComponents.kt
+++ app/src/main/java/com/example/loginflow/components/AppComponents.kt
@@ -1,5 +1,6 @@
 package com.example.loginflow.components
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -7,6 +8,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,10 +20,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -162,3 +166,17 @@ fun PasswordTextFieldComponent(labelValue: String, painterResource: Painter) {
     )
 }
 
+@Composable
+fun CheckboxComponent(value: String) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .heightIn(56.dp)
+        .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+        ) {
+
+        Checkbox(checked = false, onCheckedChange = null)
+        Text(text = stringResource(id = R.string.description), fontSize = 15.sp)
+    }
+
+}
