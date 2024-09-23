9b560a82a582e4a9df309709f146aea6c2c60ff7
Shivangi <shivangit1111@gmail.com>
2024-09-23 12:20:00 +0530

    feat: create new screen

---
+++ app/src/main/java/com/example/loginflow/screens/TermsAndConditionScreen.kt
@@ -0,0 +1,33 @@
+package com.example.loginflow.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.loginflow.R
+import com.example.loginflow.components.HeadingTextComponent
+
+@Composable
+fun TermsAndConditionScreen() {
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .padding(16.dp)
+    ) {
+        HeadingTextComponent(value = stringResource(id = R.string.termscondition_header))
+    }
+}
+
+@Preview
+@Composable
+fun defaultPreviewScreen() {
+    TermsAndConditionScreen()
+}
