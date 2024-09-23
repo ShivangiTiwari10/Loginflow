f0b546b8e8d9631c4653120b8257b1130b486fc7
Shivangi <shivangit1111@gmail.com>
2024-09-23 12:46:43 +0530

    feat: create router for navigation

--- app/src/main/java/com/example/loginflow/app/PostOfficeApp.kt
+++ app/src/main/java/com/example/loginflow/app/PostOfficeApp.kt
@@ -1,24 +1,39 @@
 package com.example.loginflow.app
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import com.example.loginflow.R
-import com.example.loginflow.components.NormalTextComponent
+import com.example.loginflow.navigation.PostOfficeAppRouter
+import com.example.loginflow.navigation.Screen
 import com.example.loginflow.screens.SignUpScreen
+import com.example.loginflow.screens.TermsAndConditionScreen
 
 @Composable
-fun PostOfficeApp(){
-Surface(
-    modifier = Modifier.fillMaxSize()
-    ,
-    color = Color.White
+fun PostOfficeApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
 
-) {
-    SignUpScreen()
-}
+    ) {
+
+        Crossfade(targetState = PostOfficeAppRouter.currentScreen, label = "") { currentState ->
+
+            when (currentState.value) {
+
+                is Screen.SignUpScreen -> {
+                    SignUpScreen()
+                }
+
+                is Screen.termAndConditionScren -> {
+                    TermsAndConditionScreen()
+                }
+            }
+
+        }
+
+//        SignUpScreen()
+    }
 }
